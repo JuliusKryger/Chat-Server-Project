1107ac40cd2882e39d0adb6af6058b523a7eee08
Annika Rosenvinge Jespersen <Annika2000@live.dk>
2021-03-09 09:57:51 +0100

    Changes

--- src/Client.java
+++ src/Client.java
@@ -5,27 +5,22 @@ public class Client extends Thread {
 
     public static void main(String[] args) throws IOException {
         //adress = IP-adressen, localhost for local connection. - and port is the port you connect through.
-        new Client().connect("localhost", 5555);
+        new Client().connect("localhost", 9090);
     }
 
     public void connect(String address, int port) throws IOException {
         //grants access to the server
         Socket accessSocket = new Socket(address, port);
 
-        // ClientHandler clientHandler = new ClientHandler(accessSocket); ... fik vist ødelagt denneher.
-
         //To read and write to the server
         BufferedReader input = new BufferedReader(new InputStreamReader(accessSocket.getInputStream()));
         BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
         PrintWriter toServer = new PrintWriter(accessSocket.getOutputStream(), true);
-
-<<<<<<< HEAD
         boolean running = true;
-        while (running) {
-=======
+
         while (running){
             System.out.println("Welcome to our virtual server");
->>>>>>> AlekBranch
+
             System.out.println("type 'stop' to end connection");
             System.out.println("----------");
 
@@ -37,8 +32,6 @@ public class Client extends Thread {
             System.out.println("FROM SERVER :" + serverResponse);
 
         }
-
-
 
 
         accessSocket.close();
--- src/Server.java
+++ src/Server.java
@@ -1,37 +1,21 @@
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 public class Server {
-
-    public static final int DEFAULT_PORT = 5555;
+    public static int port = 9090;
     ConcurrentHashMap<Integer, ClientHandler> allClientHandlers;
 
     public static void main(String[] args) throws IOException {
-        int port = DEFAULT_PORT;
         if (args.length == 1) {
             try {
                 port = Integer.parseInt(args[0]);
             } catch (NumberFormatException e) {
-                System.out.println("ERROR: Invalid port number, using default port :" + DEFAULT_PORT);
+                System.out.println("ERROR: Invalid port number, using default port :" + port);
             }
         }
         new Server().startServer(port);
-    }
-
-    void sendToAll(String msg) {
-        allClientHandlers.values().forEach(clientHandler -> {
-            try {
-                clientHandler.msgToAll(msg);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        });
     }
 
     private void startServer(int port) throws IOException {
@@ -51,32 +35,3 @@ public class Server {
         }
     }
 }
-
-/*
-public class Server {
-
-    private static int port = 9090;
-    private static boolean running = true;
-
-    private static List<ClientHandler> clients = new ArrayList<>();
-    private static ExecutorService threadPool = Executors.newFixedThreadPool(4);
-
-    public static void main(String[] args) throws IOException {
-        //client connection
-        ServerSocket clientSocket= new ServerSocket(port);
-
-        while (running) {
-            //client connection
-            System.out.println("Waiting for client to connect");
-            Socket client = clientSocket.accept();
-            System.out.println("Server connected to client");
-            ClientHandler clientThread = new ClientHandler(client);
-            clients.add(clientThread);
-            threadPool.execute(clientThread);
-        }
-
-
-    }
-}
- */
-

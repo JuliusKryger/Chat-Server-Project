9a899aa444d59ae0b47b6977029552d6db6c8078
Julius <PsychedelicCoder@users.noreply.github.com>
2021-03-10 17:00:42 +0100

    added a ClientHandlerTest

--- src/test/java/ClientHandlerTest.java
+++ src/test/java/ClientHandlerTest.java
@@ -1,59 +1,44 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingDeque;
+import java.util.concurrent.BlockingQueue;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ClientHandlerTest {
 
+    Server server;
+    ClientHandler clientHandler;
+    BlockingQueue<String> allMessages;
+    ServerSocket serverSocket;
+    Thread testThread;
+    int port = 5555;
+
     @BeforeEach
-    void setUp() {
+    void setUp() throws IOException {
+        serverSocket = new ServerSocket(port);
+        Socket socket = serverSocket.accept();
+        allMessages = new ArrayBlockingQueue<>(200);
+        String SimulatedUserInput = "Lorem Ipsum dolor sit amet, consectetur adipiscing elit.";
+        allMessages.add(SimulatedUserInput);
+        //PrintWriter pw = new PrintWriter(System.out);
+        //BufferedReader br = new BufferedReader(new StringReader(SimulatedUserInput));
+        clientHandler = new ClientHandler(socket, server, allMessages);
+        testThread = new Thread(clientHandler);
     }
 
     @Test
-    void getId() {
-    }
-
-    @Test
-    void run() {
-    }
-
-    @Test
-    void protocol() {
-    }
-
-    @Test
-    void msgToOne() {
-    }
-
-    @Test
-    void msgToAll() {
-    }
-
-    @Test
-    void clientGreeting() {
-    }
-
-    @Test
-    void testToString() {
-    }
-
-    @Test
-    void seeUsers() {
-    }
-
-    @Test
-    void clientGoodBye() {
-    }
-
-    @Test
-    void getToClient() {
-    }
-
-    @Test
-    void getName() {
-    }
-
-    @Test
-    void getClientId() {
+    void testClientSendMsg() throws InterruptedException {
+    testThread.start();
+    testThread.join();
+        System.out.println(allMessages);
     }
 }
